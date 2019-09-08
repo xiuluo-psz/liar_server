553b31099768642c688ab6578c9c7d09ed15e241
psz <hwgzpsz@126.com>
2019-09-08 11:06:58 +0800

    init project

---
+++ src/main/java/com/liar/server/LiarServerApplication.java
@@ -0,0 +1,13 @@
+package com.liar.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiarServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiarServerApplication.class, args);
+	}
+
+}
