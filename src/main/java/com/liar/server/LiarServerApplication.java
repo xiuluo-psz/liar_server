5e0ce379dd9ceea85625b3974784957c59ed177d
psz <hwgzpsz@126.com>
2019-09-08 11:19:16 +0800

    bing map

--- src/main/java/com/liar/server/LiarServerApplication.java
+++ src/main/java/com/liar/server/LiarServerApplication.java
@@ -1,9 +1,11 @@
 package com.liar.server;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.liar.server.mapper")
 public class LiarServerApplication {
 
 	public static void main(String[] args) {
