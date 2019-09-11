795c918f5756e24b44031fb7b73cfbd93e1e93d7
psz <hwgzpsz@126.com>
2019-09-11 23:55:44 +0800

    update token refresh logic

--- src/main/java/com/liar/server/controller/TokenController.java
+++ src/main/java/com/liar/server/controller/TokenController.java
@@ -1,11 +1,17 @@
 package com.liar.server.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.liar.server.annotation.Authentication;
+import com.liar.server.constant.Constants;
 import com.liar.server.constant.Status;
 import com.liar.server.entity.UserEntity;
 import com.liar.server.model.ResultModel;
@@ -18,9 +24,19 @@ public class TokenController {
 	@Autowired
 	private TokenService tokenService;
 
+	@Authentication
 	@PostMapping(value = "/refresh")
-	public ResultModel refreshToken(UserEntity user) {
+	public ResultModel refreshToken(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+			@RequestBody UserEntity user) {
 		ResultModel result = new ResultModel();
+
+		String token = httpServletRequest.getHeader(Constants.AUTHORIZATION);
+		String userId = tokenService.getUUID(token);
+		if (!userId.equals(user.getUserId())) {
+			result.setCode(Status.CODE_FAILED);
+			result.setMsg(Status.MSG_DELFAILED);
+			return result;
+		}
 		result.setCode(Status.CODE_SUCCESS);
 		result.setMsg(Status.MSG_SUCCESS);
 		result.setData(tokenService.refreshToken(user));
